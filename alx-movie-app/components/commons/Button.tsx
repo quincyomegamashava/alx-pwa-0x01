8bee9ea1a6eb2a0685fe5c738ec721131261e1da
QuincyMashava <mashavaquincy@gmail.com>
2025-08-10 22:11:03 +0200

    updated movie app components

--- alx-movie-app/components/commons/Button.tsx
+++ alx-movie-app/components/commons/Button.tsx
@@ -1,20 +1,11 @@
-import React from 'react';
+import { ButtonProps } from "@/interfaces";
 
-// Define the props for the button component for type safety.
-// You can expand this with props like `onClick`, `children`, etc.
-interface ButtonProps {
-  // Add props here
-}
-
-/**
- * A reusable button component.
- */
-const Button: React.FC<ButtonProps> = () => {
+const Button: React.FC<ButtonProps> = ({ title, action }) => {
   return (
-    <button className="px-4 py-2 font-bold text-white transition-colors duration-200 rounded-md bg-blue-500 hover:bg-blue-600">
-      Button
+    <button onClick={action} className="px-8 py-2 border-2 border-[#E2D609] rounded-full hover:bg-[#E2D609] hover:text-black transition-colors duration-300">
+      {title}
     </button>
-  );
-};
+  )
+}
 
 export default Button;
--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import { ComponentProps } from "@/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
 
-interface LayoutProps {
-  children: React.ReactNode;
-}
-
-/**
- * A layout component that wraps the entire application with a header and footer.
- */
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<ComponentProps> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-800">
+    <>
       <Header />
-      <main className="container flex-grow p-4 mx-auto">
-        {children}
-      </main>
+      <main className="min-h-screen">{children}</main>
       <Footer />
-    </div>
-  );
-};
+    </>
+  )
+}
 
 export default Layout;
