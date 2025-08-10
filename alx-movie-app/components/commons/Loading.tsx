9d2884c21a0fbb77c5daddbea1d2911982ff3454
QuincyMashava <mashavaquincy@gmail.com>
2025-08-10 22:15:48 +0200

    updated items

--- alx-movie-app/components/commons/Loading.tsx
+++ alx-movie-app/components/commons/Loading.tsx
@@ -1,17 +1,12 @@
-import React from 'react';
-
-// Define the props for the loading component.
-interface LoadingProps {
-  // Add props here
-}
-
-/**
- * A reusable loading spinner component.
- */
-const Loading: React.FC<LoadingProps> = () => {
+const Loading: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-full">
-      <div className="w-16 h-16 ease-linear border-4 border-t-4 border-gray-200 rounded-full loader animate-spin"></div>
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+      <div className="bg-white bg-opacity-10 backdrop-blur-md rounded-lg p-8 flex flex-col justify-center items-center">
+        <h1 className="text-4xl md:text-6xl font-semibold text-white mb-4 animate-pulse">
+          Loading...
+        </h1>
+        <p className="text-lg text-gray-300">Please wait, we&apos;re getting next set of movies ready for you.</p>
+      </div>
     </div>
   );
 };
--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -1,20 +1,19 @@
-import React from 'react';
+import { MovieProps } from "@/interfaces"
+import Image from "next/image"
 
-// Define the props for the movie card component.
-interface MovieCardProps {
-  // Add props like movie details here
+const MovieCard: React.FC<MovieProps> = ({ title, posterImage, releaseYear }) => {
+  return (
+    <div className="h-[563px]">
+      <div>
+        <Image className="h-[430px] w-full rounded-md hover:cursor-pointer" src={posterImage} width={100} height={100} alt={title} />
+
+      </div>
+      <div className="flex justify-between py-4">
+        <p className="text-xl font-bold">{title}</p>
+        <p className="text-xl text-[#E2D609]">{releaseYear}</p>
+      </div>
+    </div>
+  )
 }
 
-/**
- * A reusable card component to display movie information.
- */
-const MovieCard: React.FC<MovieCardProps> = () => {
-  return (
-    <div className="p-4 overflow-hidden duration-300 transform rounded-md shadow-lg cursor-pointer bg-gray-800 hover:scale-105">
-      <h3 className="mb-2 text-xl font-semibold text-white">Movie Title</h3>
-      <p className="text-gray-400">Movie description or details.</p>
-    </div>
-  );
-};
-
-export default MovieCard;
+export default MovieCard
