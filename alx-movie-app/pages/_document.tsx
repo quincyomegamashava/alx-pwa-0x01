b6716a71ef02772b5f32d646517a72e6a77fa937
QuincyMashava <mashavaquincy@gmail.com>
2025-08-24 20:16:39 +0200

    push task 3

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Link to PWA manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Theme color for status bar / address bar */}
+        <meta name="theme-color" content="#0070f3" />
+
+        {/* PWA Icons */}
+        <link rel="icon" href="/icons/android-chrome-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
